e75c9ebc1bb33fc5c96e21f043da7c6921d1c7af
tawny-cu <tawny-cu@gmx.com>
2025-03-06 15:24:00 +0000

    Add 1.py

---
+++ src/1.py
@@ -0,0 +1,17 @@
+import random
+
+def get_random_python_code():
+    python_code = """
+# This is a basic template for creating a new Python script
+
+# Import necessary libraries
+import math
+
+# Define a function to calculate the area of a rectangle
+def calculate_area(width, height):
+    return width * height
+
+# Test the function with some values
+print("The area of a rectangle with width 5 and height 3 is", calculate_area(5, 3))
+"""
+    return python_code
